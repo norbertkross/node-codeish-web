0efcdd43392c8492ec346491f3f6f1302d4f7b60
Norbert Aberor <38046968+norbertkross@users.noreply.github.com>
2020-05-06 21:22:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -161,6 +161,6 @@ app.get('*', function(req, res){
 const myhost = process.env.HOST || 'localhost'
 // connect With HTTP and listen On Port 3000 or Available PORT
     http.listen(
-      process.env.PORT || 80,myhost,()=>{
-      console.log("running on port 80");
+      process.env.PORT || 5000,myhost,()=>{
+      console.log("running on port 5000");
     })
