2d0628066fef68778ee8cd294cbfc7a888619072
norbertkross <norbertaberor@gmail.com>
2020-05-08 10:38:58 +0000

    another commit

--- RequestsData/CoursesAndLessons/lessons.js
+++ RequestsData/CoursesAndLessons/lessons.js
@@ -4,9 +4,12 @@ const mysqlConnection = require("../../MysqlConnection/connections");
 
 
 lessonsRouter.post("/lessons",(req,res,err)=>{
-  var courseid = req.query.courseid
+  var courseid = req.query.courseid;
+  console.log(`The CourseID:: ${courseid}`);
   var query1 = `SELECT * FROM Lessons WHERE courseid = '${courseid}' ORDER BY lessonid LIMIT 2000`;
   var query2 = `SELECT * FROM Lessons ORDER BY lessonid LIMIT 2000`;
+
+  console.log(`The Query 1 :: ${query1}`);
   mysqlConnection.query(
     courseid == null?query2:query1,
     //`SELECT * FROM Lessons WHERE courseid = '${courseid}'`,
