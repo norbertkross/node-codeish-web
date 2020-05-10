6736166c40c4ae6aa387d3cc3dcb0b4933865610
norbertkross <norbertaberor@gmail.com>
2020-05-10 00:03:12 +0000

    launch link in articles in new tab

--- website/flutter_service_worker.js
+++ website/flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {
 "assets/youtube.png": "fc36174c988eb3c628c7d4267d498efd",
 "favicon.ico": "00f60d999e16e77adbffd7a2f74508ff",
 "index.html": "22642fabe28949d18988b9d963a4650e",
-"main.dart.js": "98d8676a0b8a00ef826347d25677a797"
+"main.dart.js": "c40fd5ba4e3aaeaed416025b87578e0a"
 };
 
 self.addEventListener('activate', function (event) {
