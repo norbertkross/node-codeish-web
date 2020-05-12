564874d7aac7093bf6c0dfef084ffda109155695
norbertkross <norbertaberor@gmail.com>
2020-05-12 14:59:45 +0000

    login and minor UI

--- website/flutter_service_worker.js
+++ website/flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {
 "assets/youtube.png": "fc36174c988eb3c628c7d4267d498efd",
 "favicon.ico": "00f60d999e16e77adbffd7a2f74508ff",
 "index.html": "22642fabe28949d18988b9d963a4650e",
-"main.dart.js": "2a0c499d39dc3351270f41437d6ebf1c"
+"main.dart.js": "229f9df7635f8b714511aab48fe973a1"
 };
 
 self.addEventListener('activate', function (event) {
